869635e3da0f1a19a8be65015d0a6379ba268990
Markiem <nyjc35sc.marcus@gmail.com>
2017-04-07 12:55:41 +0800

    Update ValidId.py

--- src/ValidId.py
+++ src/ValidId.py
@@ -1,10 +1,12 @@
 def check_id(identity)
 # identity should be either from CardRead or Userinput
-    firebase.get("/") database_id =
+    firebase.get("/", "ID", DictID) database_id =
     # need set to retrieve value from firebase and store as local variable
     # database ID should be a dictionary
     
     if identity in database_id.keys()
+    
+        
         return True
 
     else:
