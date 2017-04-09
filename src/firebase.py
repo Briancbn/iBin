b856d2a4109746cb3672dadfa24f3ff5a24c9472
Markiem <nyjc35sc.marcus@gmail.com>
2017-04-09 23:03:45 +0800

    Delete firebase.py

--- src/firebase.py
+++
@@ -1,7 +0,0 @@
-### Communications with the firebase ###
-from firebase import firebase
-
-url = "https://ddigitalworld-20118.firebaseio.com/" #unique to project
-token = "Czk1K09ZNHtLLOfRLdIe6htAzeWwrqYwJTnbrOqN" #unique token used for authentication
-
-firebase = firebase.FirebaseApplication(url, token)
